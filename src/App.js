1491763d84b2cd4137cb233f9d29942ff6e30244
Grant Kirill <webdotg.inbox@gmail.com>
2023-05-07 16:05:20 +0300

    nav link add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import { Home } from './pages/Homepage'
+import { Blog } from './pages/Blogpage'
+import { About } from './pages/Aboutpage'
+import { Notfound } from './pages/Notfoundpage'
+import { Layout } from "./components/Layout";
+
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' element={<Layout/>}  >
+          <Route index path="/" element={<Home />} />
+          <Route path="posts" element={<Blog />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<Notfound />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
